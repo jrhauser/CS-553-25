2ca60d62e2c744558628955b11d8b844149d21d9
jrhauser <jrh00045@mix.wvu.edu>
2025-03-18 19:59:51 -0400

    early work on adjancency matrix and getting distance vectors working

---
+++ include/distance-node.h
@@ -0,0 +1,7 @@
+
+const int MAX_TABLE_SIZE = 5;
+
+typedef struct {
+    int router_id;
+    int distance_vector[5];
+} distNode;
--- src/distance-vector.c
+++ src/distance-vector.c
@@ -1,3 +1,21 @@
+#include <adj.h>
+#include <distance-node.h>
+#include <stdio.h>
+
+
+void pop_vector(distNode* node);
+
 int main(void) {
-    printf("Hello World!");
+    distNode nodes[5];
+    for (int i = 0; i < MAX_TABLE_SIZE; i++) {
+        nodes[i].router_id = i;
+        pop_vector(&nodes[i]);
+    }
+}
+
+
+void pop_vector(distNode* node){
+    for (int i = 0; i < MAX_TABLE_SIZE; i++) {
+        node->distance_vector[i] = adj[node->router_id][i];
+    }
 }
